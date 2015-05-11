3de92a024802acafc769e7ec1bbd6d3867dea577
Ravi Surulikamu <rsurulik@mvlnxrsurulikvm>
2015-05-11 11:04:03 -0700

    Enhanced logging & hardcoded support to terminate client when 1 MB of data is used

--- server.go
+++ server.go
@@ -1,24 +1,33 @@
 package main
 
 import (
-	"net"
-	"fmt"
 	"io"
 	"log"
+	"net"
+)
+
+const (
+	listenPort   = "2345"
+	maxEchoBytes = 1024 * 1024 // 1 MB
 )
 
 func acceptConnection(c net.Conn) {
-	fmt.Println("Accepted connection", c)
-	io.Copy(c, c)
-	fmt.Println("Terminating connection", c)
+	log.Println("Accepted connection from", c.RemoteAddr())
+	n, err := io.CopyN(c, c, maxEchoBytes)
+	if err != nil && err != io.EOF {
+		log.Print("Early termination:", err)
+	}
+	log.Println("Terminating connection from", c.RemoteAddr(),
+		"after echoing", n, "bytes")
 	c.Close()
 }
 
 func main() {
-	ln, err := net.Listen("tcp", ":2345")
+	ln, err := net.Listen("tcp", ":"+listenPort)
 	if err != nil {
 		log.Fatal("Failed to listen: ", err)
 	}
+	log.Print("Echo server listening on", ln.Addr())
 	defer ln.Close()
 	for {
 		c, err := ln.Accept()
